f67fbb50a8b856a5802182bd9bda771ee92fe842
Den4200 <dpham.42@hotmail.com>
2020-02-03 17:03:02 -0500

    scanner is complete

---
+++ src/core/compiler/compiler.h
@@ -0,0 +1,6 @@
+#ifndef flux_compiler_h
+#define flux_compiler_h
+
+void compile(const char* source);
+
+#endif
--- src/core/vm/vm.c
+++ src/core/vm/vm.c
@@ -2,6 +2,7 @@
 
 #include "../common.h"
 #include "vm.h"
+#include "../compiler/compiler.h"
 
 #include "../debugger/debug.h"
 
@@ -96,8 +97,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-	vm.chunk = chunk;
-	vm.ip = vm.chunk->code;
-	return run();
+InterpretResult interpret(const char* source) {
+	compile(source);
+	return INTERPRET_OK;
 }
