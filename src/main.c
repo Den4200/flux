c3a5d97790c3ef86d919e59ca715235846c154f6
Den4200 <dpham.42@hotmail.com>
2020-02-02 23:04:55 -0500

    created basic virtual machine to run instructions on a stack

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "core/common.h"
 #include "core/chunk.h"
-#include "debugger/debug.h"
+#include "core/vm/vm.h"
+#include "core/debugger/debug.h"
 
 int main(int argc, const char* argv[]) {
+	initVM();
+	
 	Chunk chunk;
 	initChunk(&chunk);
 
@@ -10,9 +13,26 @@ int main(int argc, const char* argv[]) {
 	writeChunk(&chunk, OP_CONSTANT, 123);
 	writeChunk(&chunk, constant, 123);
 
+	constant = addConstant(&chunk, 3.4);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+
+	writeChunk(&chunk, OP_ADD, 123);
+
+	constant = addConstant(&chunk, 5.6);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+
+	writeChunk(&chunk, OP_DIVIDE, 123);
+
+	writeChunk(&chunk, OP_NEGATE, 123);
+
 	writeChunk(&chunk, OP_RETURN, 123);
 
 	disassembleChunk(&chunk, "test chunk");
+	interpret(&chunk);
+
+	freeVM();
 	freeChunk(&chunk);
 	return 0;
 }
